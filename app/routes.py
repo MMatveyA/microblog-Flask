dc8ec432b21ee21a4d3f3e6ed6ee1882f117ed8a
Maximov Matvey <maximov-matvi@yandex.ru>
2022-07-15 23:33:04 +0400

    Добавлена функция регистрации на сайте

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
-from app import app
-from app.forms import LoginForm
+from werkzeug.urls import url_parse
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
 
 
@@ -9,7 +10,6 @@ from app.models import User
 @app.route('/index')
 @login_required
 def index():
-    user = {'username': 'Matvey'}
     posts = (
         {
             'author': {'username': 'John'},
@@ -20,7 +20,7 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     )
-    return render_template('index.html', user=user, posts=posts)
+    return render_template('index.html', posts=posts)
 
 @app.route('/help')
 def help():
@@ -47,3 +47,18 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
